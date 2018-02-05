97de425bf0413cb9393a023a27c176a8d3cbbdc8
dillon9 <eater1234@gmail.com>
2018-02-05 12:07:36 -0600

    shift reload chrome thanks ez

--- index.php
+++ index.php
@@ -10,24 +10,10 @@
         <title>Premier Scandinavia</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="bootstrap.min.css">
     </head>
     <body>
-        <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
-  </div>
-</nav>
+        <header><?php require "navbar.php";?></header>
+
     </body>
 </html>
