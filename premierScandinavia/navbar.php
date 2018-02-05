7ae818e840ceff52c4debb84457360feb21c2d85
dillon9 <eater1234@gmail.com>
2018-02-05 12:13:54 -0600

    reorganized files

---
+++ premierScandinavia/navbar.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" type="text/css" href="css/navbar_styles.css">
+<div class="nav">
+	<div class="inner">
+	<ul>
+        <li>
+            <a href="index.php">Home</a>
+        </li>
+        <li>
+            <a href="sale.php">Search By Country</a>
+            <ul>
+                <li><a href="">Sweden</a></li>
+                <li><a href="">Norway</a></li>
+                <li><a href="">Denmark</a></li>
+            </ul>
+            
+        </li>
+        <li>
+            <a href="">Generic 2</a>
+
+        </li>
+        <li>
+            <a href="">Generic 3</a>
+        </li>
+        <li>
+            <a href="">Sign up</a>
+            <ul>
+                <li><a href="">Log in</a></li>
+            </ul>
+        </li>
+	</ul>
+</div>
+<br style="clear:both">
